c582e773e4f32de266f3f49b7f9ec2f9141e01ae
J Berber <JB@MacBook-Pro-de-J.local>
2013-03-25 13:22:17 -0700

    Initial Commit

---
+++ Login/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Login
+//
+//  Created by J Berber on 25/03/13.
+//  Copyright (c) 2013 Canilu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Login/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Login
+//
+//  Created by J Berber on 25/03/13.
+//  Copyright (c) 2013 Canilu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
